952e5e687abfa04f40d3da58b0350a637cdf3159
ilaiyagithub <il50@pcl.co.uk>
2016-06-15 16:32:45 +0100

    Add files via upload

--- tst.js
+++ tst.js
@@ -1,14 +1,31 @@
-var test = require('tape');
+var validCharsNoSpaceRegex = /^[0-9a-z!@#\$%'\(\)\*\+,\-\.\/:;<=>\?\[\\\]\^_`\{\|}~]+$/i;
+var validCharsWithSpaceRegex = /^[ 0-9a-z!@#\$%'\(\)\*\+,\-\.\/:;<=>\?\[\\\]\^_`\{\|}~]+$/i;
 
-test('basic arithmetic', function (t) {
-    t.plan(3);
 
-    t.equal(2 + 3, 5);
-    t.equal(7 * 8 + 9, 65);
-    t.notEqual(5,6,'5 and 6 are not equal','extra');
-});
+var text = {
+  max: function max(length) {
+    return function (value) {
+      return value !== '' && value.length > Number(length) ? 'error':'';
+    };
+  },
+  min: function min(length) {
+    return function (value) {
+      return value !== '' && value.length < Number(length) ? [{ error: i18n('fieldWarnings:valueShort') }] : [];
+    };
+  },
+  noDigits: function noDigits(value) {
+    return (/\d/.test(value) ? [{ error: i18n('fieldWarnings:noDigits') }] : []
+    );
+  },
+  validChars: function validChars(allowSpaces) {
+    return function (value) {
+      var regex = allowSpaces === 'true' ? validCharsWithSpaceRegex : validCharsNoSpaceRegex;
+      return !regex.test(value) ? [{ error: i18n('fieldWarnings:invalidChars') }] : [];
+    };
+  },
+  max1: length => value => value !== '' && value.length > Number(length) ?
+    'Valdiation fails!': ''
+};
 
-test('My Test', t => {
-    t.plan(1);
-    t.equal(2 + 3, 5);
-});
+console.log(text.max(3)('ddsssd'));
+console.log(text.max1(3)('ddsssd'));
