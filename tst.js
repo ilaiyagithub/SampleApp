836526516b57379be21bf81b75ccc143168eb20c
ilaiyagithub <il50@pcl.co.uk>
2016-06-16 14:40:50 +0100

    few changes

--- tst.js
+++ tst.js
@@ -1,31 +1,8 @@
-var validCharsNoSpaceRegex = /^[0-9a-z!@#\$%'\(\)\*\+,\-\.\/:;<=>\?\[\\\]\^_`\{\|}~]+$/i;
-var validCharsWithSpaceRegex = /^[ 0-9a-z!@#\$%'\(\)\*\+,\-\.\/:;<=>\?\[\\\]\^_`\{\|}~]+$/i;
+//Eslint test
+var aa=10;
+console.log(aa); // no error for console.log
+var abc=20;
 
 
-var text = {
-  max: function max(length) {
-    return function (value) {
-      return value !== '' && value.length > Number(length) ? 'error':'';
-    };
-  },
-  min: function min(length) {
-    return function (value) {
-      return value !== '' && value.length < Number(length) ? [{ error: i18n('fieldWarnings:valueShort') }] : [];
-    };
-  },
-  noDigits: function noDigits(value) {
-    return (/\d/.test(value) ? [{ error: i18n('fieldWarnings:noDigits') }] : []
-    );
-  },
-  validChars: function validChars(allowSpaces) {
-    return function (value) {
-      var regex = allowSpaces === 'true' ? validCharsWithSpaceRegex : validCharsNoSpaceRegex;
-      return !regex.test(value) ? [{ error: i18n('fieldWarnings:invalidChars') }] : [];
-    };
-  },
-  max1: length => value => value !== '' && value.length > Number(length) ?
-    'Valdiation fails!': ''
-};
+//Promise tst
 
-console.log(text.max(3)('ddsssd'));
-console.log(text.max1(3)('ddsssd'));
