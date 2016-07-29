0d392a92ae98db426e2b88b9a7736fe037cb5b24
ilaiya <il50@pcl.co.uk>
2016-07-29 17:35:13 +0100

    workout

--- tst.js
+++ tst.js
@@ -3,12 +3,6 @@ var aa=10;
 console.log(aa); // no error for console.log
 var abc=20;
 
-let myStuff = [
-    {name: 'henrik'}
-]
 
-myStuff = [...mystuff, {name: 'js lovin fool'}]
-
-console.log(myStuff[0].name);
 
 
